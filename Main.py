80f9ca5b0368274830aecb805a9a466fee5599ca
fl1kers <161144727+fl1kers@users.noreply.github.com>
2024-07-06 17:29:18 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import pygame
+from time import sleep
 
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed):
@@ -18,9 +19,9 @@ fi=0
 mo="l"
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Maze")
-background = pygame.transform.scale(pygame.image.load("jungle.png"), (width, height))
+background = pygame.transform.scale(pygame.image.load("background.webp"), (width, height))
 
-player = GameSprite('hero.png', 318, 218, 4)
+player = GameSprite('hero.png', 20, 408, 4)
 monster = GameSprite('cyborg.png', 620, 280, 2)
 final = GameSprite('treasure.png', 580, 420, 0)
  
@@ -30,8 +31,22 @@ a = 1
 pygame.mixer.init()
 pygame.mixer.music.load('jungle.mp3')
 pygame.mixer.music.play()
- 
+kick = pygame.mixer.Sound("kick.ogg")
+money = pygame.mixer.Sound("money.ogg")
+color_wall = 99,11,111
+wall1 = pygame.Rect(200,220,20,350)
+wall2 = pygame.Rect(270,300,240,20)
+wall3 = pygame.Rect(-20,300,130,20)
+wall4 = pygame.Rect(10,2,800,20)
+wall5 = pygame.Rect(400,170,500,20)
+wall6 = pygame.Rect(220,300,230,20)
+wall7 = pygame.Rect(340,270,20,50)
+wall8 = pygame.Rect(200,140,20,150)
+wall9 = pygame.Rect(220,140,75,20)
+wall10 = pygame.Rect(180,140,75,20)
+pens = [wall1,wall2,wall3,wall4,wall4,wall5,wall6,wall7,wall8,wall9,wall10]
 while game:
+   
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             game = False
@@ -45,6 +60,8 @@ while game:
         player.rect.y -= 5
     if keys[pygame.K_DOWN] and player.rect.y < 430:
         player.rect.y += 5
+    if keys[pygame.K_n]:
+        monster.rect.y += 50
     
     if fi!=80 and mo=="l":
         monster.rect.x-=2.5
@@ -60,11 +77,26 @@ while game:
         fi=0
         mo="l"
         
+    
+
 
     window.blit(background,(0, 0))
     player.draw()
     monster.draw()
     final.draw()
-
+    for i in pens:
+        pygame.draw.rect(window, color_wall, i)
+        if player.rect.colliderect(i):
+            kick.play()
+            sleep(1)
+            game = False
+        if player.rect.colliderect(monster):
+            kick.play()
+            sleep(0.1)
+            game = False
+        if player.rect.colliderect(final):
+            money.play()
+            sleep(0.25)
+            game = False
     pygame.display.update()
     clock.tick(60)
